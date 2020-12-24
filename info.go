559648d522a4f33abec82bcbc98ec165b1f96df5
MakeevD <dmitry.makeev@snowball.dev>
2020-12-24 13:04:44 +0300

    IsDev() helper function

--- info.go
+++ info.go
@@ -1,5 +1,11 @@
 package slice
 
+import "strings"
+
+const (
+	devPrefix string = "dev"
+)
+
 // The application information.
 type Info struct {
 	// The Application name.
@@ -9,4 +15,9 @@ type Info struct {
 	Env Env
 	// The debug flag.
 	Debug bool
+}
+
+// IsDev is a helper function to check whether app is running in Dev mode
+func (info *Info) IsDev() bool {
+	return strings.HasPrefix(strings.ToLower(string(info.Env)), devPrefix)
 }
